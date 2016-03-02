a01198920f053e46ab475f0140e34a760e4d5be8
Sushil Singh <sushilks@gmail.com>
2016-03-02 15:51:32 -0800

    Cleaned up the test class

---
+++ docs/README.zmq.md
@@ -0,0 +1,11 @@
+ZMQ Client scale testing.
+
+Still working on identifying what to test at scale, will keep all the ideas here for tracking
+PUB-SUB
+ > Measure the client join rate and the latency to join under heavy client load.
+ > Measure X(Max message rate without drop at PUB) Y(Number of clients as SUB)
+
+ > Add Client HA(Hard Disconnect) on 10% of the client and see the behaviour of PUB.
+
+ Client-Server
+ >
--- src/main/python/mScale/zmqTest/run_test.py
+++ src/main/python/mScale/zmqTest/run_test.py
@@ -8,89 +8,62 @@ from marathon.models import MarathonApp, MarathonConstraint
 import netifaces
 import time
 import logging
-from mScale.lib import testAppServer, mmAPI, util
+from mScale.lib import testAppServer, mmAPI, util, runTestBase
 
 
 l = util.createLogger('runTest', logging.INFO)
 # l.setLevel(logging.DEBUG)
 
 
-def RunTest():
-    config = ConfigParser()
-    if len(sys.argv) == 2:
-        config.read(sys.argv[1])
-    else:
-        config.read('zst.ini')
-    myport = config.getint('zst', 'port')
-    myserver = testAppServer.TServer(myport, 'live')
-    myserver.start()
-    mydev = config.get('zst', 'dev')
-    myip = netifaces.ifaddresses(mydev)[2][0]['addr']
-    myaddr = 'http://' + myip + ':' + str(myport)
+class RunTest(runTestBase.RunTestBase):
+    def __init__(self):
+        config = ConfigParser()
+        if len(sys.argv) == 2:
+            config.read(sys.argv[1])
+        else:
+            config.read('zst.ini')
+        runTestBase.RunTestBase.__init__(self, 'zmqScale', config)
 
-    # now init the marathon plugin and create a app for this path
-    mesos_addr = 'http://' + config.get('mesos', 'ip') + ':' + \
-                 config.get('mesos', 'port')
-    marathon_addr = 'http://' + config.get('marathon', 'ip') + ':' + \
-                    config.get('marathon', 'port')
-    mesos = mmAPI.MesosIF(mesos_addr)
-    mt = mmAPI.MarathonIF(marathon_addr, myip, mesos)
-    # delete the running application
-    l.info("Delete any pre-existing apps")
-    mt.deleteAppIfExisting('/zst-pub')
-    mt.deleteAppIfExisting('/zst-sub')
-    # wait for the application to be removed
-    l.info("Waiting for delete to complete")
-    mt.waitForAppRemoval('/zst-pub')
-    mt.waitForAppRemoval('/zst-sub')
-    l.info("Populating the app on the server")
-    os.system("rm ../live/zmq_scale_test.tgz")
-    os.system("cd .. && tar cfz live/zmq_scale_test.tgz target src build.py")
-    l.info("Launching the pub app")
-    mt.createApp('/zst-pub',
+        zstPub = '/zst-pub'
+        zstSub = '/zst-sub'
+        self.addAppID(zstPub)
+        self.addAppID(zstSub)
+        self.startInit()
+        l.info("Launching the pub app")
+        self.mt.createApp(zstPub,
                  MarathonApp(cmd='chmod +x ./run_test.sh && ./run_test.sh pub 1555',
                              cpus=0.01,
                              mem=32,
                              constraints=[MarathonConstraint(field='hostname', operator='UNIQUE')],
-                             uris=[myaddr + '/zmq_scale_test.tgz', myaddr + '/run_test.sh']))
+                             uris=[self.getAppURI(), self.getAppServerAddr() + '/run_test.sh']))
 
-    # wait for the application to be launched and be ready
-    a1 = mt.waitForAppReady('/zst-pub', 1)
-    taskIP = None
-    for task in a1.tasks:
-        l.info("TASK " + task.id + " Running on host : " + task.host + ' IP = ' + mesos.getSlaveIPFromHN(task.host))
-        taskIP = mesos.getSlaveIPFromHN(task.host)
+        # wait for the application to be launched and be ready and find it's IP
+        taskIP = self.findIPforUniqueAPP(zstPub)
 
-    # now we can launch subscribe app with ip port
-    mt.createApp('/zst-sub',
+        # now we can launch subscribe app with ip port
+        self.mt.createApp(zstSub,
                  MarathonApp(cmd="chmod +x ./run_test.sh && ./run_test.sh sub %s 1555" % taskIP,
                              cpus=0.01,
                              mem=32,
-                             uris=[myaddr + '/zmq_scale_test.tgz', myaddr + '/run_test.sh']))
+                             uris=[self.getAppURI(), self.getAppServerAddr() + '/run_test.sh']))
 
-    a2 = mt.waitForAppReady('/zst-sub', 1)
-    l.info("Done with launching the pub and sub processes, will scale the sub side now")
-    scale = 100
-    mt.scaleApp('/zst-sub', scale)
-    l.info('Done with starting of scaling the app to %d' % scale)
+        a2 = self.mt.waitForAppReady(zstSub, 1)
+        l.info("Done with launching the pub and sub processes, will scale the sub side now")
+        scale = 100
+        self.mt.scaleApp('/zst-sub', scale)
+        l.info('Done with starting of scaling the app to %d' % scale)
 
-    cnt = 0
-    while True:
-        a2 = mt.getApp('/zst-sub')
-        l.info('[%d] Application count running = %d, staged = %d' % (cnt, a2.tasks_running, a2.tasks_staged))
-        if (a2.tasks_running >= scale):
-            break
-        cnt += 1
-        sys.stdout.flush()
-        time.sleep(1)
-    l.info("All the tasks are running now. press Ctrl-C to exit.")
-
-    try:
-        while 1:
+        cnt = 0
+        while True:
+            a2 = self.mt.getApp('/zst-sub')
+            l.info('[%d] Application count running = %d, staged = %d' % (cnt, a2.tasks_running, a2.tasks_staged))
+            if (a2.tasks_running >= scale):
+                break
+            cnt += 1
             sys.stdout.flush()
             time.sleep(1)
-    except KeyboardInterrupt:
-        l.info("Exiting")
-        myserver.stop()
-        myserver.join()
-    pass
+        l.info("All the tasks are running now. press Ctrl-C to exit.")
+        self.waitForInterrupt()
+
+
+
