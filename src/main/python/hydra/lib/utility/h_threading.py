122b2e3ccd7470742d0aa222e4406d5e7970fb0a
Abdullah Shah <shah.abdullah.syed@gmail.com>
2016-03-06 04:29:39 +0000

    Cleanup a bit
    Signed-off-by: Abdullah Shah <shah.abdullah.syed@gmail.com>

--- src/main/python/hydra/lib/utility/h_threading.py
+++ src/main/python/hydra/lib/utility/h_threading.py
@@ -19,24 +19,10 @@ class HThread(object):
         self.thread = None  # This will be used to hold threading.Thread object
 
     def _get_my_tid(self):
-        """
-        determines this (self's) thread id
-        i-e self.ident
-        """
         return self.thread.ident
         raise AssertionError("could not determine the thread's id")
 
     def _start_thread(self, callback_fn, fn, daemon=False, **kwargs):
-        """
-        Starts a thread :)
-        args:
-        callback_fn : Callback function to be invoked in case the thread raises
-                      User MUST provide a call back function to catch exceptions
-                      This will prevent thread exceptions to be lost !
-        fn :          Function to execute in the thread
-        kwargs :      Args to thread function
-        daemon:       Flag to start thread in daemon mode. Default is false.
-        """
         thread_kwargs = kwargs.pop('kwargs', {})
         thread_args = kwargs.pop('args', ())
         wait_event = kwargs.pop('event', ())
@@ -46,15 +32,9 @@ class HThread(object):
         self.thread.start()
 
     def _is_alive(self):
-        """
-        Check if current thread is alive
-        """
         return self.thread.is_alive()
 
     def _wrapper(self, fn, args, kwargs, callback_fn):
-        """
-        Function to be used internally by the class to wrap a user function
-        """
         try:
             fn(*args, **kwargs)
         except:
@@ -74,16 +54,6 @@ class HThreading(object):
         self.thread_id_fn = {}
 
     def start_thread(self, callback_fn, fn, daemon=False, **kwargs):
-        """
-        Starts a thread :)
-        args:
-        callback_fn : Callback function to be invoked in case the thread raises
-                      User MUST provide a call back function to catch exceptions
-                      This will prevent thread exceptions to be lost !
-        fn :          Function to execute in the thread
-        kwargs :      Args to thread function
-        daemon:       Option to run thread in daemon mode. Default is false.
-        """
         t = HThread()
         t._start_thread(callback_fn, fn, daemon, **kwargs)
         self.running_threads.append(t)
@@ -91,9 +61,6 @@ class HThreading(object):
         return t
 
     def join_all(self, timeout=None):
-        """
-        Wait for all threads to return
-        """
         t_list = []
         for t in self.running_threads:
             t.thread.join(timeout)
@@ -103,25 +70,3 @@ class HThreading(object):
 
         for t in t_list:
             self.running_threads.remove(t)
-
-    def all_alive(self):
-        """
-        Check if all threads are alive
-        """
-        t_list = []
-        for t in self.running_threads:
-            if t._is_alive():
-                t_list.append(t)
-        if len(t_list) == len(self.running_threads):
-            return True
-        else:
-            return False
-
-    def any_alive(self):
-        """
-        Check if any thread is alive
-        """
-        for t in self.running_threads:
-            if t._is_alive():
-                return True
-        return False
