687ccd4d03807e6cf964a4b50f0c94a7533063c9
tahir24434 <tahir.rauf1@gmail.com>
2016-05-25 19:06:19 -0700

    Minor fix
    Signed-off-by: tahir24434 <tahir.rauf1@gmail.com>

--- src/main/scripts/setup/vm_files/hydra_pkgs_install.sh
+++ src/main/scripts/setup/vm_files/hydra_pkgs_install.sh
@@ -4,9 +4,9 @@ dst_work_dir=$1
 echo "**** $dst_work_dir"
 # Pre Script
 echo "Run pre script"
-sudo apt-get -y install python-dev python-pip protobuf-c-compiler libzmq3-dev protobuf-compiler libprotobuf-dev
 wget https://raw.githubusercontent.com/zeromq/cppzmq/master/zmq.hpp
 mv zmq.hpp ${dst_work_dir}/hydra-master/src/main/c/zmq
+sudo apt-get install python-dev python-pip
 
 echo "Setup virtual environemnt"
 # Setup virtual environment
@@ -17,6 +17,7 @@ virtualenv ${venv_dir}
 source ${venv_dir}/bin/activate
 
 echo "Install Hydra"
+sudo apt-get -y install protobuf-c-compiler libzmq3-dev protobuf-compiler libprotobuf-dev
 pip install pybuilder
 pushd ${dst_work_dir}/hydra-master
 pyb install_dependencies
