c12d3be1928d8a6019616f1ee6f97169c5651dde
Abdullah Shah <shah.abdullah.syed@gmail.com>
2016-06-01 00:23:05 +0000

    Final
    Signed-off-by: Abdullah Shah <shah.abdullah.syed@gmail.com>

--- src/main/python/hydra/lib/runtestbase.py
+++ src/main/python/hydra/lib/runtestbase.py
@@ -334,7 +334,6 @@ class RunTestBase(BoundaryRunnerBase):
             info = self.apps[name]['ip_port_map'][task_id]
             port = info[0]
             ip = info[1]
-            l.info(port)
             ha = HAnalyser(ip, port, task_id)
             # Signal it to start sending data, blocks until PUB responsds with "DONE" after sending all data
             res = ha.do_ping()
@@ -370,7 +369,6 @@ class RunTestBase(BoundaryRunnerBase):
         """ Refresh all the ip-port map for the application
         This is done by talking to marathon and getting the list of tasks
         """
-        l.info("REFRESH ===========================")
         assert(name in self.apps)
         self.apps[name] = {'ip_port_map': {},
                            'stats': {},
@@ -383,7 +381,6 @@ class RunTestBase(BoundaryRunnerBase):
                 ip_port_map[task.id + '_PORT' + str(app_rep_port)] = \
                     [app_rep_port, app_ip]
         self.all_task_ids[name] = self.apps[name]["ip_port_map"].keys()
-        l.info(self.all_task_ids)
         return len(tasks)
 
     def fetch_app_stats(self, name, group_name=""):
@@ -400,7 +397,6 @@ class RunTestBase(BoundaryRunnerBase):
         """
         assert(name in self.apps)
         task_list = self.all_task_ids[name]
-        l.info(task_list)
         if group_name:
             assert(group_name in self.app_group)
             task_list = self.app_group[group_name]
@@ -414,8 +410,6 @@ class RunTestBase(BoundaryRunnerBase):
             info = self.apps[name]['ip_port_map'][task_id]
             port = info[0]
             ip = info[1]
-            l.info(info)
-            l.info(port)
             ha_sub = HAnalyser(ip, port, task_id)
             # Signal it to start sending data, blocks until PUB responsds with "DONE" after sending all data
             stats = ha_sub.get_stats()
